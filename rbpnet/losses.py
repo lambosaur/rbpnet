da8c46ed01ad893f7eb212995a754051159cd5e2
Lambert Moyon <lambert.moyon@helmholtz-munich.de>
2023-12-03 16:37:58 +0100

    Handle NaN in loss calculation.

--- rbpnet/losses.py
+++ rbpnet/losses.py
@@ -5,21 +5,21 @@ import gin
 
 # %%
 # Register external functions with gin
-gin.config.external_configurable(tf.math.log1p, module='tf.math')
+gin.config.external_configurable(tf.math.log1p, module="tf.math")
+
 
 # %%
-@gin.configurable(denylist=['true_counts', 'logits'])
+@gin.configurable(denylist=["true_counts", "logits"])
 @tf.function
-def multinomial_loss(true_counts, logits, post_fn = None):
-
-    #return tf.reduce_sum(logits)
+def multinomial_loss(true_counts, logits, post_fn=None):
+    # return tf.reduce_sum(logits)
 
     """
-    Compute the multinomial negative log-likelihood along the sequence (axis=1). 
+    Compute the multinomial negative log-likelihood along the sequence (axis=1).
 
     Args:
-      true_counts: observed count values (batch, seqlen, channels)
-      logits: predicted logit values (batch, seqlen, channels)
+        true_counts: observed count values (batch, seqlen, channels)
+        logits: predicted logit values (batch, seqlen, channels)
     """
 
     # expected shape: (batch_size, input_length)
@@ -29,15 +29,27 @@ def multinomial_loss(true_counts, logits, post_fn = None):
     true_counts = tf.cast(true_counts, tf.float32)
     logits = tf.cast(logits, tf.float32)
 
+    # Replace NaN values with zeros
+    true_counts = tf.where(
+        tf.math.is_finite(true_counts), true_counts, tf.zeros_like(true_counts)
+    )
+    logits = tf.where(tf.math.is_finite(logits), logits, tf.zeros_like(logits))
+
     total_counts = tf.reduce_sum(true_counts, axis=-1)
 
     # expected shape: (batch_size, )
     tf.debugging.assert_rank(total_counts, 1)
 
     dist = tfp.distributions.Multinomial(total_count=total_counts, logits=logits)
+    # Replace NaN values with zeros in the log_prob calculation
+    log_prob = tf.where(
+        tf.math.is_finite(dist.log_prob(true_counts)),
+        dist.log_prob(true_counts),
+        tf.zeros_like(true_counts),
+    )
 
-    loss = -tf.reduce_mean(dist.log_prob(true_counts))
+    loss = -tf.reduce_mean(log_prob)
     if post_fn is not None:
-     loss = post_fn(loss)
+        loss = post_fn(loss)
 
     return loss
